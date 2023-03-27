16db54e6cfe3d76d6a547dfe9eede87969e93774
Locus_Natit <3113135718@qq.com>
2023-03-27 18:36:07 +0800

    fix bug

--- src/main/java/org/infernalstudios/miningmaster/init/MMLootModifiers.java
+++ src/main/java/org/infernalstudios/miningmaster/init/MMLootModifiers.java
@@ -16,6 +16,7 @@
 
 package org.infernalstudios.miningmaster.init;
 
+import com.google.common.collect.Lists;
 import com.google.gson.JsonObject;
 import net.minecraft.inventory.Inventory;
 import net.minecraft.item.ItemStack;
@@ -33,6 +34,7 @@ import net.minecraftforge.registries.ForgeRegistries;
 import org.infernalstudios.miningmaster.MiningMaster;
 
 import javax.annotation.Nonnull;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
@@ -56,7 +58,8 @@ public class MMLootModifiers {
         @Nonnull
         @Override
         protected List<ItemStack> doApply(List<ItemStack> generatedLoot, LootContext context) {
-            for (ItemStack item : generatedLoot) {
+            List<ItemStack> tempLoot = new ArrayList<>(generatedLoot);
+            for (ItemStack item : tempLoot) {
                 Optional<FurnaceRecipe> optional = context.getWorld().getRecipeManager().getRecipe(IRecipeType.SMELTING, new Inventory(item), context.getWorld());
                 if (optional.isPresent()) {
                     ItemStack itemstack = optional.get().getRecipeOutput();
